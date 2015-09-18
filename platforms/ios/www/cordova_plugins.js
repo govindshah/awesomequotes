10c1b6fe509068571b040cc1b5c6422c529df31c
Govind Shah <gshah1@gmail.com>
2015-09-18 15:21:10 -0400

    Added GA plugin - https://github.com/appfeel/analytics-google

--- platforms/ios/www/cordova_plugins.js
+++ platforms/ios/www/cordova_plugins.js
@@ -13,13 +13,21 @@ module.exports = [
         "clobbers": [
             "window.plugins.AppleAdvertising"
         ]
+    },
+    {
+        "file": "plugins/com.analytics.google/www/analytics.js",
+        "id": "com.analytics.google.GAPlugin",
+        "merges": [
+            "analytics"
+        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
     "com.ionic.keyboard": "1.0.4",
-    "com.aquto.cordova.AppleAdvertising": "0.0.1"
+    "com.aquto.cordova.AppleAdvertising": "0.0.1",
+    "com.analytics.google": "1.2.2"
 }
 // BOTTOM OF METADATA
 });
