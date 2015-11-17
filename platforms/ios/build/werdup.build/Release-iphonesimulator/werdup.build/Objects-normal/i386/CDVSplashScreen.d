9a9bd254c72dda8b2e2819d22854bc42093bde9f
Govind Shah <gshah1@gmail.com>
2015-11-17 14:51:57 -0500

    additional generated files

---
+++ platforms/ios/build/werdup.build/Release-iphonesimulator/werdup.build/Objects-normal/i386/CDVSplashScreen.d
@@ -0,0 +1,14 @@
+dependencies: \
+  /Users/govind/workspace/awesomequotes/platforms/ios/werdup/Plugins/org.apache.cordova.splashscreen/CDVSplashScreen.m \
+  /Users/govind/workspace/awesomequotes/platforms/ios/werdup/Plugins/org.apache.cordova.splashscreen/CDVSplashScreen.h \
+  /Users/govind/workspace/awesomequotes/platforms/ios/build/emulator/include/Cordova/CDVPlugin.h \
+  /Users/govind/workspace/awesomequotes/platforms/ios/build/emulator/include/Cordova/CDVPluginResult.h \
+  /Users/govind/workspace/awesomequotes/platforms/ios/build/emulator/include/Cordova/CDVAvailability.h \
+  /Users/govind/workspace/awesomequotes/platforms/ios/build/emulator/include/Cordova/CDVAvailabilityDeprecated.h \
+  /Users/govind/workspace/awesomequotes/platforms/ios/build/emulator/include/Cordova/NSMutableArray+QueueAdditions.h \
+  /Users/govind/workspace/awesomequotes/platforms/ios/build/emulator/include/Cordova/CDVCommandDelegate.h \
+  /Users/govind/workspace/awesomequotes/platforms/ios/build/emulator/include/Cordova/CDVInvokedUrlCommand.h \
+  /Users/govind/workspace/awesomequotes/platforms/ios/build/emulator/include/Cordova/CDVViewController.h \
+  /Users/govind/workspace/awesomequotes/platforms/ios/build/emulator/include/Cordova/CDVCommandQueue.h \
+  /Users/govind/workspace/awesomequotes/platforms/ios/build/emulator/include/Cordova/CDVWhitelist.h \
+  /Users/govind/workspace/awesomequotes/platforms/ios/build/emulator/include/Cordova/CDVScreenOrientationDelegate.h
